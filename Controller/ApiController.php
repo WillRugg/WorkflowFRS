39aab2794575a6a8dd2991f63791f5402d919a81
NOYER Nadine <noyer.nadine@comeca.intra>
2017-07-28 16:10:51 +0200

    api m3 encours de dev => Will a finir

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -17,6 +17,19 @@ class ApiController extends Controller {
 		require('View/Index/testApi.php') ; 
 	}
 
+	public function creefrsM3Action() {
+		$app_title="Fournisseur TEST  " ;
+		$app_desc="Comeca" ;
+		$app_body="body_Cree" ;
+		
+		// liste des clients 
+		require_once('Model/ApiM3Model.php');
+		$apiModel = new ApiM3Model();
+				
+		$frsM3 = $apiModel->creerfrsM3($this->post);
+		 
+		require('View/Index/testApi.php') ; 
+	}
 		
 	 
 	
